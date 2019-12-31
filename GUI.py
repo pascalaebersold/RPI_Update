36637601e57252d55b0b141cc6c1de32d1c8f001
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2019-12-31 17:39:40 +0100

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -53,28 +53,28 @@ exitbutton = Button(text = "exit", command = close_window)
 # define font for Button
 exitbutton['font'] = FontHEL20B
 # define place on window
-exitbutton.place(x=w/2-w100*2.5, y=h-h100*6)
+exitbutton.place(x=w/2-w100*2.5, y=h-h100*5)
 
 # Button to go to the left window
 leftbutton = Button(text = "<", command = left_window, bd=0)
 # define font for Button
 leftbutton['font'] = FontHEL50B
 # define place on window
-leftbutton.place(x=0, y=h/2-h100*4)
+leftbutton.place(x=0, y=h/2-h100*2)
 
 # Button to go to the right window
 rightbutton = Button(text = ">", command = right_window, bd=0)
 # define font for Button
 rightbutton['font'] = FontHEL50B
 # define place on window
-rightbutton.place(x=w-w100*6, y=h/2-h100*4)
+rightbutton.place(x=w-w100*6, y=h/2-h100*2)
 
 # Button to close the window
 rightbutton = Button(text = "UPDATE", command = update_program, bd=0)
 # define font for Button
 rightbutton['font'] = FontHEL50B
 # define place on window
-rightbutton.place(x=w/2-w100*15, y=h/2-h100*4)
+rightbutton.place(x=w/2-w100*30, y=h/2-h100*2)
 
 # mainloop for tkinter
 root.mainloop()
