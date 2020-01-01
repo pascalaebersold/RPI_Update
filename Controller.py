32c5ae59008d07f78181d1e8d45f5a415a274508
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2020-01-01 15:35:07 +0100

    Add files via upload

--- Controller.py
+++ Controller.py
@@ -26,7 +26,8 @@ def close_window ():
 
 # class who exits the window
 def connect_controller ():
-    os.spawnl(os.P_DETACH, "sudo ds4drv")
+    #os.spawnl(os.P_DETACH, "sudo ds4drv")
+    os.system("sudo ds4drv")
 
 # class who exits the window
 def start ():
