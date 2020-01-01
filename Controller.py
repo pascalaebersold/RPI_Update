4c862e1d187c433a96fa5deec11d120b62beca22
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2020-01-01 15:13:35 +0100

    Add files via upload

--- Controller.py
+++ Controller.py
@@ -2,66 +2,20 @@ from tkinter import *
 import tkinter.font as font
 import os
 import pygame
+import sys
+
+a=".."
 
 root = Tk()
 
-#initializes pygame
-pygame.init()
-
-#creats a controller object
-controller = pygame.joystick.Joystick(1)
-
-#initializes the controller
-controller.init()
-
-def scale_servo(x):
-
-        # used to scale -1,1 to 0,180
-        y = round((30-70)*x+1/1+1+70,2)
-
-        return y
-
-try:
-    while True:
-        events = pygame.event.get()
-        for event in events:
-            angle = scale_servo(controller.get_axis(0))
-            steering.angle = angle
-            print("Angle: {}".format(angle))
-            if event.type == pygame.JOYBUTTONDOWN:
-                if controller.get_button(0):
-                    root.destroy()
-                elif controller.get_button(1):
-                    print("Circle Pressed")
-                elif controller.get_button(2):
-                    print("Triangle Pressed")
-                elif controller.get_button(3):
-                    print("Square Pressed")
-                elif controller.get_button(4):
-                    print("L1 Pressed")
-                elif controller.get_button(5):
-                    print("R1 Pressed")
-                elif controller.get_button(6):
-                    print("L2 Pressed")
-                elif controller.get_button(7):
-                    print("R2 Pressed")
-                elif controller.get_button(8):
-                    print("SHARE Pressed")
-                elif controller.get_button(9):
-                    print("OPTIONS Pressed")
-                elif controller.get_button(10):
-                    print("Power Button Pressed")
-                elif controller.get_button(11):
-                    print("Left Analog Pressed")
-                elif controller.get_button(12):
-                    print("Right Analog Pressed")
-
-            elif event.type == pygame.JOYBUTTONUP:
-                print("Button Released")
-
-except KeyboardInterrupt:
-    print("EXITING NOW")
-    controller.quit()
+os.system("sudo ds4drv")
+pygame.joystick.init() #find the joysticks
+joy = pygame.joystick.Joystick(0)
+joy.init()
+if(joy.get_name()=='Sony Entertainment Wireless Controller'):
+    a="DS4 connected"
+else:
+    a="Not a DS4"
 
 # define font
 FontHEL50B = font.Font(family='Helvetica', size=30, weight='bold')
