8347df8ae28182a534f61267e4ef9dd3590f7931
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2020-01-01 21:00:16 +0100

    Add files via upload

--- Controller_connected.py
+++ Controller_connected.py
@@ -1,17 +1,42 @@
 import pygame
 from time import sleep
+import serial
+import time
+import RPi.GPIO as GPIO
+import math
 
-screen = pygame.display.set_mode([10,10]) #make a 10x10 window
-pygame.joystick.init() #find the joysticks
+# define the serial port (UART)
+port = serial.Serial("/dev/ttyAMA0", baudrate=1000000, timeout=3.0)
+
+#make a 10x10 window
+screen = pygame.display.set_mode([10,10])
+#find the joysticks
+pygame.joystick.init()
 joy = pygame.joystick.Joystick(0)
 joy.init()
 
-while True:
+def send_UART:
+    GPIO.output(17, GPIO.HIGH)
+    port.write(arrt)
+    time.sleep(0.1)
+    port.write(arrs)
+    time.sleep(0.1)
+    port.write(arra)
+    time.sleep(1)
+    GPIO.output(17, GPIO.LOW)
+
+while C > 0:
     pygame.event.get()
     rt = joy.get_axis(5)
     lt = joy.get_axis(2)
     X = joy.get_button(1)
-    print(rt)
-    print(lt)
-    print(X)
+    C = joy.get_button(2)
+    PS4 = joy.get_button(12)
+
+    if PS4 > 0:
+        arra = (255, 255, 5, 5, 3, 30, 0, 0, 212)
+        arrs = (255, 255, 5, 5, 3, 32, 15, 0, 195)
+        arrt = (255, 255, 5, 5, 3, 34, 119, 1, 88)
+        send_UART()
+
     sleep(0.1) #limit the frequency to 10Hz
