6d3617b528bb21a0a06750cebe82b040d800a0dc
pascalaebersold <46541784+pascalaebersold@users.noreply.github.com>
2020-01-13 21:00:51 +0100

    Add files via upload

--- Controller_Mode.py
+++ Controller_Mode.py
@@ -9,6 +9,7 @@ C = 0
 
 # sets the 74ls24 to send mode
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
 
 # define the serial port (UART)
@@ -22,18 +23,14 @@ joy = pygame.joystick.Joystick(0)
 joy.init()
 
 def send_UART ():
-    strs = bytearray(arrs)
-    strt = bytearray(arrt)
-    stre = bytearray(arre)
-    stra = bytearray(arra)
     GPIO.output(17, GPIO.HIGH)
-    port.write(strs)
+    port.write(arrs)
     time.sleep(0.1)
-    port.write(strt)
+    port.write(arrt)
     time.sleep(0.1)
-    port.write(stre)
-    time.sleep(0.1)
-    port.write(stra)
+    #port.write(arre)
+    #time.sleep(0.1)
+    port.write(arra)
     time.sleep(0.1)
     GPIO.output(17, GPIO.LOW)
 
@@ -45,13 +42,14 @@ while C > 0:
     C = joy.get_button(2)
     PS4 = joy.get_button(12)
 
+    # If the Playstationbutton is pressed the arm will go to it's initial position
     if PS4 > 0:
         #array speed sets the speed for the next action
         arrs = (255, 255, 4, 5, 3, 32, 8, 0, 203)
         #array torque sets the torque for the next action
         arrt = (255, 255, 4, 5, 3, 34, 255, 3, 207)
         #array enable sets the motor to listen mode so that he can execute a movment
-        arre = (255, 255, 4, 2, 5, 24, 0, 220)
+        #arre = (255, 255, 4, 2, 5, 24, 0, 220)
         #array angle sets the angle/position
         arra = (255, 255, 4, 5, 3, 30, 130, 1, 82)
         send_UART()
@@ -60,7 +58,7 @@ while C > 0:
         #array torque sets the torque for the next action
         arrt = (255, 255, 3, 5, 3, 34, 255, 3, 208)
         #array enable sets the motor to listen mode so that he can execute a movment
-        arre = (255, 255, 3, 2, 5, 24, 0, 221)
+        #arre = (255, 255, 3, 2, 5, 24, 0, 221)
         #array angle sets the angle/position
         arra = (255, 255, 3, 5, 3, 34, 255, 3, 208)
         send_UART()
@@ -69,7 +67,7 @@ while C > 0:
         #array torque sets the torque for the next action
         arrt = (255, 255, 2, 5, 3, 34, 255, 3, 209)
         #array enable sets the motor to listen mode so that he can execute a movment
-        arre = (255, 255, 2, 2, 5, 24, 0, 222)
+        #arre = (255, 255, 2, 2, 5, 24, 0, 222)
         #array angle sets the angle/position
         arra = (255, 255, 2, 5, 3, 30, 160, 2, 53)
         send_UART()
@@ -78,9 +76,7 @@ while C > 0:
         #array torque sets the torque for the next action
         arrt = (255, 255, 1, 5, 3, 34, 0, 2, 210)
         #array enable sets the motor to listen mode so that he can execute a movment
-        arre = (255, 255, 1, 2, 5, 24, 0, 223)
+        #arre = (255, 255, 1, 2, 5, 24, 0, 223)
         #array angle sets the angle/position
         arra = (255, 255, 1, 5, 3, 30, 0, 0, 216)
         send_UART()
-
-    sleep(0.1) #limit the frequency to 10Hz
